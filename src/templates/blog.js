69bbb8a234580ee0fe6fbfcdb250f788c9c1f7ff
mzulzidan <mzulzidan@gmail.com>
2023-05-30 20:29:14 +0800

    update for seo blog

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -4,6 +4,7 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
 import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import Layout from '../components/layout';
+import SEOHead from "../components/head";
 
 const Bold = ({ children }) => <span className="font-bold">{children}</span>
 const Text = ({ children }) => <p className="">{children}</p>
@@ -25,6 +26,35 @@ const options = {
   },
 }
 
+const generateExcerpt = (rawContent) => {
+  const content = JSON.parse(rawContent)
+  let plainText = ''
+
+  const extractText = (node) => {
+    if (node.nodeType === 'text') {
+      plainText += node.value.trim() + ' '
+    }
+
+    if (node.content) {
+      node.content.forEach(extractText)
+    }
+  }
+
+  content.content.forEach(extractText)
+
+  plainText = plainText
+    .replace(/\n/g, '') // Remove newlines
+    .replace(/\s+/g, ' ') // Replace multiple whitespaces with a single space
+    .trim()
+
+  const maxLength = 150 // Set the maximum number of characters for the excerpt
+  if (plainText.length <= maxLength) {
+    return plainText
+  }
+  return `${plainText.slice(0, maxLength)}...`
+}
+
+
 const BlogPagesComponents = ({ data, location }) => {
   const { title, content, featuredMedia } = data.contentfulBlog
   const image = getImage(featuredMedia.gatsbyImageData)
@@ -32,14 +62,19 @@ const BlogPagesComponents = ({ data, location }) => {
   return (
     <Layout location={location}>
       <div className="px-4 py-8 sm:px-8 md:px-16 lg:px-20  prose max-w-none">
-<div className='mb-24'>        {image && <GatsbyImage image={image} alt={featuredMedia.title} />}</div>
+        <div className='mb-24'>
+          {image && <GatsbyImage image={image} alt={featuredMedia.title} />}
+        </div>
         <h1 className="text-3xl font-bold mb-4">{title}</h1>
         {content && renderRichText(content, options)}
       </div>
     </Layout>
   )
 }
-
+export const Head = ({data}) => {
+  const { title, content, featuredMedia } = data.contentfulBlog
+  return <SEOHead title={title} description={generateExcerpt(content.raw) } image={featuredMedia} />
+}
 export default BlogPagesComponents
 
 export const query = graphql`
