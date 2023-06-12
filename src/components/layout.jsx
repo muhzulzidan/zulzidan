c466a90c34614afd3d50d0fab2a7c64d9c91708e
mzulzidan <mzulzidan@gmail.com>
2023-06-12 22:06:40 +0800

    make sure footer will show on path that i chose

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-// import { GatsbySeo } from "gatsby-plugin-next-seo";
-import { useSiteMetadata } from "../hooks/use-site-metadata";
-import { SEO } from "./seo";
 import Header from "./header";
 import Footer from "./footer";
 
@@ -15,61 +12,20 @@ const Layout = ({ title, pathname, children, location }) => {
     "/works",
     "/works/",
   ];
-  const showFooter = location.location
-    ? paths.includes(location.location.pathname)
-    : paths.includes(location.pathname);
 
-  const { title: defaultTitle, siteUrl } = useSiteMetadata();
-  const seo = {
-    title: title || defaultTitle,
-    // description: description || defaultDescription,
-    // image: `${siteUrl}${image}`,
-    url: `${siteUrl}${pathname || ``}`,
-    // twitterUsername,
-  };
+  const isBlogPage = location?.location?.pathname?.startsWith("/blog") || false;
+  const isBlogPage2 = location?.pathname?.startsWith("/blog") || false;
+  const showFooter = paths.includes(location?.location?.pathname) || paths.includes(location?.pathname) || isBlogPage || isBlogPage2;
+
   return (
     <div className="layout">
-      {console.log(title)}
-      {/* <SEO title={title} pathname={pathname}  /> */}
-      {/* <GatsbySeo
-        title="Using More of Config"
-        description="This example uses more of the available config options."
-        canonical="https://www.canonical.ie/"
-        openGraph={{
-          url: "https://www.url.ie/a",
-          title: "Open Graph Title",
-          description: "Open Graph Description",
-          images: [
-            {
-              url: "https://www.example.ie/og-image-01.jpg",
-              width: 800,
-              height: 600,
-              alt: "Og Image Alt",
-            },
-            {
-              url: "https://www.example.ie/og-image-02.jpg",
-              width: 900,
-              height: 800,
-              alt: "Og Image Alt Second",
-            },
-            { url: "https://www.example.ie/og-image-03.jpg" },
-            { url: "https://www.example.ie/og-image-04.jpg" },
-          ],
-          site_name: "SiteName",
-        }}
-        twitter={{
-          handle: "@handle",
-          site: "@site",
-          cardType: "summary_large_image",
-        }}
-      /> */}
+      {/* {console.log(showFooter)}
+      {console.log(location)} */}
       <Header />
       {children}
       {showFooter && <Footer />}
     </div>
   );
 };
-
-
 
 export default Layout;
