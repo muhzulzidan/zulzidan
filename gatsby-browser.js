ea7333acf6fa1bfa9919f36da054866f13682b6c
muhzulzidan <mzulzidan@gmail.com>
2024-08-22 17:24:46 +0800

    Initial commit from gatsby

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,1 @@
-
-import "./src/style/index.scss";
-// import "@fontsource/questrial/latin-400.css";
-// import "@fontsource/poppins/latin-400.css";
-// import "@fontsource/poppins/latin-500.css";
-// import "@fontsource/poppins/latin-600.css";
-// import "@fontsource/poppins/latin-700.css";
-
-
+import "./src/styles/global.css"
