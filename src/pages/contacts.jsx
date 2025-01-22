9f2c4036e11220acfe3e416531993e39526189c9
muhzulzidan <mzulzidan@gmail.com>
2025-01-22 15:21:15 +0800

    fix: Update email address in Contacts page for collaboration inquiries

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -14,7 +14,7 @@ const ContactsPages = (location) => {
                 <h1 style={{display:"none"}} >Contacts</h1>
                 <h2>Let us Collaborate</h2>
 
-                {!isTable ? (<MailTo email="mail@muhzulzidan.my.id" subject="Hello, I Want to Collaborate" body="Hello, i am" ><p>zulzdn@gmail.com</p></MailTo>) : (<a href="https://mail.google.com/mail/u/0/?fs=1&to=mail@muhzulzidan.my.id&su=Hello,+I+Want+to+Collaborate&body&bcc=%22&tf=cm"><p>zulzdn@gmail.com</p></a> )}
+                {!isTable ? (<MailTo email="mail@zulzidan.com" subject="Hello, I Want to Collaborate" body="Hello, i am" ><p>zulzdn@gmail.com</p></MailTo>) : (<a href="https://mail.google.com/mail/u/0/?fs=1&to=mail@zulzidan.com&su=Hello,+I+Want+to+Collaborate&body&bcc=%22&tf=cm"><p>zulzdn@gmail.com</p></a> )}
                 
                 <div className="findMe">
                     <h2>Find me on :</h2>
